377eb200f96b3eb6b1256251ba34cd1701af9d73
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-01-31 13:39:05 -0800

    added comments

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 'use strict';
 
+// What is your name?
 const name = prompt('What is your name?');
 console.log('their name; ' + name);
 alert('Hello ' + name + '!');
 
+// What is your favorite band?
 const favBand = prompt('What is your favorite band?');
 console.log('their favorite band; ' + favBand);
 alert('Nice choice, ' + name + '. I like ' + favBand + ' too.');
 
+//If you were in this band what instrument would you play?
 const instrument = prompt('If you were in this band what instrument would you play?');
 console.log('their instrument; ' + instrument);
 alert('Well, ' + name + ' it looks like you play the ' + instrument + ' for ' + favBand + '!');
 
+//Now that you are part of the band, where would you like to play your first gig?
 const gigLocation = prompt('Now that you are part of the band, where would you like to play your first gig?');
 console.log('gig location; ' + gigLocation);
 alert('Alright, ' + name + '! Pack your bags and your ' + instrument + '. You are off to ' + gigLocation + ' to play with ' + favBand + '!');
 
+//Have you packed your bags for the show?
 const bagsPacked = prompt('Have you packed your bags for the show?').toLowerCase();
 console.log('bags packed: ' + bagsPacked);
+
 if (bagsPacked === 'yes' || bagsPacked.startsWith('y')){
     alert('Alright, ' + name + '! Grab your ' + instrument + ' ' + gigLocation + ' awaits!');
 } else if (bagsPacked === 'no' || bagsPacked.startsWith('n')){
@@ -26,6 +32,7 @@ if (bagsPacked === 'yes' || bagsPacked.startsWith('y')){
     alert('Look, ' + name + '. I\'m going to need a yes or no from you.');
 }
 
+//Are you riding on the tour bus?
 const tourBus = prompt('Are you riding on the tour bus?').toLowerCase();
 console.log('riding tour bus: ' + tourBus);
 
@@ -37,6 +44,7 @@ if (tourBus === 'yes' || tourBus.startsWith('y')){
     alert('Do you know how you\'re getting to ' + gigLocation + '?');
 }
 
+//Have you tuned your instrument?
 const tuned = prompt('Have you tuned your instrument?').toLowerCase();
 console.log('insterment tuned: ' + tuned);
 
@@ -48,6 +56,7 @@ if (tuned === 'yes' || tuned.startsWith('y')){
     alert('Can you not tune your ' + gigLocation + '?');
 }
 
+// Have you done a sound check?
 const soundCheck = prompt('Have you done a sound check?').toLowerCase();
 console.log('done sound check: ' + soundCheck);
 
@@ -59,6 +68,7 @@ if (soundCheck === 'yes' || soundCheck.startsWith('y')){
     alert('If you\'re not sure if you\'ve done a sound check then ask a roadie.');
 }
 
+//Did you get an encore?
 const encore = prompt('Did you get an encore?').toLowerCase();
 console.log('encore: ' + encore);
 
