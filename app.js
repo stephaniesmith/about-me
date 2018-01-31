1c4319cb8b353c1910f53b8b1bc66570b73f97d2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-01-31 14:08:36 -0800

    updated questions and added lists

--- app.js
+++ app.js
@@ -3,31 +3,31 @@
 // What is your name?
 const name = prompt('What is your name?');
 console.log('their name; ' + name);
-alert('Hello ' + name + '!');
+alert('Hello ' + name + '! I\'m Stephanie.');
 
 // What is your favorite band?
 const favBand = prompt('What is your favorite band?');
 console.log('their favorite band; ' + favBand);
-alert('Nice choice, ' + name + '. I like ' + favBand + ' too.');
+alert('Nice choice, ' + name + '. I like ' + favBand + ' too. Matt & Kim is my favorite band. I bet you would be into them!');
 
 //If you were in this band what instrument would you play?
 const instrument = prompt('If you were in this band what instrument would you play?');
 console.log('their instrument; ' + instrument);
-alert('Well, ' + name + ' it looks like you play the ' + instrument + ' for ' + favBand + '!');
+alert('Well, ' + name + ' it looks like you play the ' + instrument + ' for ' + favBand + ' and I\'ll be on the tambourine!');
 
 //Now that you are part of the band, where would you like to play your first gig?
 const gigLocation = prompt('Now that you are part of the band, where would you like to play your first gig?');
 console.log('gig location; ' + gigLocation);
-alert('Alright, ' + name + '! Pack your bags and your ' + instrument + '. You are off to ' + gigLocation + ' to play with ' + favBand + '!');
+alert('Alright, ' + name + '! Pack your bags and your ' + instrument + '. We are off to ' + gigLocation + ' to play with ' + favBand + '!');
 
 //Have you packed your bags for the show?
 const bagsPacked = prompt('Have you packed your bags for the show?').toLowerCase();
 console.log('bags packed: ' + bagsPacked);
 
 if (bagsPacked === 'yes' || bagsPacked.startsWith('y')){
-    alert('Alright, ' + name + '! Grab your ' + instrument + ' ' + gigLocation + ' awaits!');
+    alert('Alright, ' + name + '! I\'m all packed up too so grab your ' + instrument + ' ' + gigLocation + ' awaits!');
 } else if (bagsPacked === 'no' || bagsPacked.startsWith('n')){
-    alert('Alright, ' + name + '. Get it together. You\'re playing ' + instrument + ' for ' + favBand.toUpperCase() + '!');
+    alert('Alright, ' + name + '. Get it together. We\'re playing ' + instrument + ' for ' + favBand.toUpperCase() + '! I\'m coming over to help you pack');
 } else {
     alert('Look, ' + name + '. I\'m going to need a yes or no from you.');
 }
@@ -37,11 +37,11 @@ const tourBus = prompt('Are you riding on the tour bus?').toLowerCase();
 console.log('riding tour bus: ' + tourBus);
 
 if (tourBus === 'yes' || tourBus.startsWith('y')){
-    alert('Awesome! You\'re going to get so much bonding time with ' + favBand + '.');
+    alert('Awesome! We\'re going to get so much bonding time with ' + favBand + '.');
 } else if (tourBus === 'no' || tourBus.startsWith('n')){
-    alert(favBand + ' got you a plane ticket to ' + gigLocation + '! I bet you\'re flying first class.');
+    alert(favBand + ' got us plane tickets to ' + gigLocation + '. We\'re flying first class!');
 } else {
-    alert('Do you know how you\'re getting to ' + gigLocation + '?');
+    alert('Do you know how you\'re getting to ' + gigLocation + '? I\'m riding on the tour bus');
 }
 
 //Have you tuned your instrument?
@@ -49,9 +49,9 @@ const tuned = prompt('Have you tuned your instrument?').toLowerCase();
 console.log('insterment tuned: ' + tuned);
 
 if (tuned === 'yes' || tuned.startsWith('y')){
-    alert('Sounding good, ' + name + '. Get your ' + instrument + ' on that stage!');
+    alert('Sounding good, ' + name + '. Get your ' + instrument + ' on that stage! We can have a pre-show jam session.');
 } else if (tuned === 'no' || tuned.startsWith('n')){
-    alert('So, your ' + instrument + ' sound like a dying cat. ' + favBand + ' will be disappointed.');
+    alert('So, your ' + instrument + ' sound like a dying cat. Bring your ' + instrument + ' over here, I\'ll help you with that.');
 } else {
     alert('Can you not tune your ' + gigLocation + '?');
 }
@@ -61,21 +61,21 @@ const soundCheck = prompt('Have you done a sound check?').toLowerCase();
 console.log('done sound check: ' + soundCheck);
 
 if (soundCheck === 'yes' || soundCheck.startsWith('y')){
-    alert(name + ', get stoked! Your set with ' + favBand + ' is going to be awesome!');
+    alert(name + ', get stoked! Our set with ' + favBand + ' is going to be awesome! I\'m ready for my tambourine solo!');
 } else if (soundCheck === 'no' || soundCheck.startsWith('n')){
     alert('Get to the stage! Got to make sure your ' + instrument + ' sounds beautiful tonight!');
 } else {
-    alert('If you\'re not sure if you\'ve done a sound check then ask a roadie.');
+    alert('If you\'re not sure if you\'ve done a sound check then ask a roadie for help.');
 }
 
 //Did you get an encore?
-const encore = prompt('Did you get an encore?').toLowerCase();
+const encore = prompt('I had to leave the show early due to a tambourine related injury. Did we get an encore?').toLowerCase();
 console.log('encore: ' + encore);
 
 if (encore === 'yes' || encore.startsWith('y')){
     alert('Congrats, ' + name + '! You\'re a rock star now.');
 } else if (encore === 'no' || encore.startsWith('n')){
-    alert('Sorry, ' + gigLocation + ' was a boring audience.');
+    alert('Sorry, ' + gigLocation + ' was a boring audience and they ruined my tambourine!');
 } else {
-    alert(name + ', it\'s either yes or no. You either got an encore or you didn\'t.');
+    alert(name + ', it\'s either yes or no. We either got an encore or we didn\'t.');
 }
